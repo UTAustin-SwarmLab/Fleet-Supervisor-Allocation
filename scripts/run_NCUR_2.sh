0466ca53a708c168c54c2ce5240de0e6ecf36afe
tanriverdiege <tanriverdiege@gmail.com>
2024-09-30 17:34:36 -0500

    Added NCUR

--- scripts/run_NCUR_2.sh
+++ scripts/run_NCUR_2.sh
@@ -1,6 +1,22 @@
-CUDA_VISIBLE_DEVICES=1 python -m main @scripts/args_anymal_NCUR.txt --logdir_suffix NCUR \
-    --seed 0 --warmup_penalty 250 --network real --allocation NCUR 
-CUDA_VISIBLE_DEVICES=2 python -m main @scripts/args_anymal_NCUR.txt --logdir_suffix NCUR \
-    --seed 0 --warmup_penalty 250 --network scarce --allocation NCUR 
-CUDA_VISIBLE_DEVICES=3 python -m main @scripts/args_anymal_NCUR.txt --logdir_suffix NCUR \
-    --seed 0 --warmup_penalty 250 --network fiveg --allocation NCUR 
+conda activate fsa
+CUDA_VISIBLE_DEVICES=5 python -m main @scripts/args_anymal_NCUR.txt --logdir_suffix NCUR_0.4_n \
+    --seed 2 --network changing --allocation NCUR \
+    --logdir /nas/oguzhan/fleet_supervision/rebuttal_logs/changing_network/anymal \
+    --connection_thresh 0.4 &
+
+
+CUDA_VISIBLE_DEVICES=6 python -m main @scripts/args_allegro_NCUR.txt --logdir_suffix NCUR_0.4_n \
+    --seed 2 --network changing --allocation NCUR \
+    --logdir /nas/oguzhan/fleet_supervision/rebuttal_logs/changing_network/allegro \
+    --warmup_penalty 500 \
+    --connection_thresh 0.4 &
+
+CUDA_VISIBLE_DEVICES=7 python -m main @scripts/args_ballbalance_NCUR.txt --logdir_suffix NCUR_0.4_n \
+    --seed 2 --network changing --allocation NCUR \
+    --logdir /nas/oguzhan/fleet_supervision/rebuttal_logs/changing_network/ballbalance \
+    --connection_thresh 0.4 &
+
+CUDA_VISIBLE_DEVICES=3 python -m main @scripts/args_humanoid_NCUR.txt --logdir_suffix NCUR_0.4_n \
+    --seed 2 --network changing --allocation NCUR \
+    --logdir /nas/oguzhan/fleet_supervision/rebuttal_logs/changing_network/humanoid \
+    --connection_thresh 0.4 &
